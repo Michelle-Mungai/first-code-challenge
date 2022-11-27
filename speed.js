e8917611c98445447370569e0736718088807683
Michelle-Mungai <chellymungai@gmail.com>
2022-11-27 16:10:24 +0300

    Added Speed Detector Code

---
+++ speed.js
@@ -0,0 +1,15 @@
+const checkSpeed = (speed) => {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+if (speed < speedLimit){
+    console.log ("Ok")
+} 
+
+const points = Math.floor((speed - speedLimit)  / kmPerPoint);
+if (points >= 12)
+console.log("License suspended");
+else console.log("Points:", points);
+}
+
+checkSpeed(300);
