c298a64614dc2d86359d2e608f410cb37a4fd657
Michelle-Mungai <chellymungai@gmail.com>
2022-11-28 19:27:36 +0300

    Speed comments added

--- speed.js
+++ speed.js
@@ -1,3 +1,5 @@
+//Speed variables declared
+
 const checkSpeed = (speed) => {
     const speedLimit = 70;
     const kmPerPoint = 5;
@@ -6,9 +8,13 @@ if (speed < speedLimit){
     console.log ("Ok")
 } 
 
+//Calculate if their license is suspended
+
 const points = Math.floor((speed - speedLimit)  / kmPerPoint);
 if (points >= 12)
 console.log("License suspended");
+// Return drivers points
+
 else console.log("Points:", points);
 }
 
